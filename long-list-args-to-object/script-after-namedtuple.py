1b83f253b6bc12c7bd0ddbe00544450b457abfac
Bob Belderbos <bobbelderbos@gmail.com>
2023-11-16 09:31:53 +0100

    code 5th video

---
+++ long-list-args-to-object/script-after-namedtuple.py
@@ -0,0 +1,14 @@
+from typing import NamedTuple
+
+class Person(NamedTuple):
+    name: str
+    age: int
+    address: str
+    phone: str
+    email: str
+
+def process_data(person: Person):
+    print(f"Processing data for {person.name}, {person.age}, living at {person.address}. Contact info: {person.phone}, {person.email}")
+
+person = Person("Alice", 30, "123 Main St", "555-1234", "alice@example.com")
+process_data(person)
---
+++ long-list-args-to-object/script-before.py
@@ -0,0 +1,4 @@
+def process_data(name, age, address, phone, email):
+    print(f"Processing data for {name}, {age}, living at {address}. Contact info: {phone}, {email}")
+
+process_data("Alice", 30, "123 Main St", "555-1234", "alice@example.com")
